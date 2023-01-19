8d70a48a93d0feb1050ab733824f57e4295f86f5
William Almeida <willliam.dev@gmail.com>
2023-01-19 19:00:11 -0300

    :art: create route to get daily habit

--- src/routes.ts
+++ src/routes.ts
@@ -30,5 +30,48 @@ export async function appRoutes(app: FastifyInstance) {
 
   })
 
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parseDate = dayjs(date).startOf("day")
+    const weekDay = parseDate.get("day")
+
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate()
+
+      }, include: {
+        dayHabits: true
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+  })
+
 
 }
