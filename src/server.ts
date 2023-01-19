0a84d4d71f2832fa70573484522e4ee2171142aa
William Almeida <willliam.dev@gmail.com>
2023-01-18 23:36:46 -0300

    :art: create first API route

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,12 @@
 import Fastify from "fastify"
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import { appRoutes } from "./routes"
+
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get("/", async () => {
-
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "code"
-      }
-    }
-  })
-
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({ port: 3000 }).then(() => {
   console.log(">> http server is running")
